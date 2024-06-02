b6fb700485ab10f08e6ef98017a7be3560477064
Suvagata <suvagata.pal@gmail.com>
2024-06-02 11:04:27 +0530

    feat: change to appid based database table partitioning

--- src/supabase.js
+++ src/supabase.js
@@ -1,21 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Create a single supabase client for interacting with your database
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_KEY,
 );
-const appId = 'app1';
+const collection = process.env.REACT_APP_SUPABASE_APP;
 
 const fetch = async (setMessages, conversation_id, page = 0, perpage = 10) => {
   console.log(`GET - ${page}`);
   const start = page * perpage;
   const end = start + 10;
   const { data, error } = await supabase
-    .from('messages')
+    .from(collection)
     .select('id, message, receiver_id, sender_id, created_at')
     .eq('conversation_id', conversation_id)
-    .eq('app_id', appId)
     .order('created_at', { ascending: false })
     .range(start, end);
   if (error) {
@@ -30,14 +28,9 @@ const subscribe = (setMessages, conversation_id) => {
     .channel('table_db_changes')
     .on(
       'postgres_changes',
-      { event: 'INSERT', schema: 'public', table: 'messages' },
+      { event: 'INSERT', schema: 'public', table: collection },
       () => fetch(setMessages, conversation_id),
     )
-    // .on(
-    //   'postgres_changes',
-    //   { event: 'UPDATE', schema: 'public', table: 'messages' },
-    //   () => fetch(setMessages, conversation_id),
-    // )
     .subscribe((payload) => {
       if (payload === 'SUBSCRIBED') {
         fetch(setMessages, conversation_id);
@@ -52,7 +45,7 @@ const unsubscribe = () => {
 };
 
 const send = async (content) => {
-  await supabase.from('messages').insert({ ...content, app_id: appId });
+  await supabase.from(collection).insert({ ...content });
 };
 
 export { subscribe, unsubscribe, send, fetch };
